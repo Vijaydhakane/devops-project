68baf4bb3b9946bd960b387812111aece0de4733
Vijaydhakane <dhakanevijay1998@gmail.com>
2025-09-10 08:49:34 +0000

    g

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_instance" "webserver" {
+  ami           = "ami-0a716d3f3b16d290c"
+  instance_type = "t3.micro"
+  key_name      = "key.pem"
+
+  tags = {
+    Name = "WebServer"
+  }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    apt update -y
+    apt install -y docker.io docker-compose git
+    systemctl enable docker
+    systemctl start docker
+  EOF
+}
